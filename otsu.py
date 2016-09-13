d46561d464a00c81c6c4b431a13ff5ea50b327a4
Jeff Maggio <jmaggio14@gmail.com>
2016-09-12 23:08:40 -0400

    Otsu's method running at roughly half a millisecond

--- otsu.py
+++ otsu.py
@@ -4,12 +4,13 @@ from os.path import split
 import ipcv
 
 def otsu_threshold(img, maxCount=255, verbose=False):
-"""
+	"""
 	:NAME:
 		otsu_threshold
 
 	:PURPOSE:
 		this method generates a binary thresholded image based off of 
+		Otsu's class discrimination method
 
 	:CATEGORY:
 		ipcv -- object recognition and thresholding tool
@@ -26,7 +27,6 @@ def otsu_threshold(img, maxCount=255, verbose=False):
 			[int] maximum pixel value in the output array
 		verbose
 			[boolean] whether or not to graph the histogram 
-				  (NOT THE NUMBER OF ACTUAL UNIQUE PIXEL VALUES)
 
 	:RETURN VALUE:
 		tuple containing:
@@ -51,83 +51,92 @@ def otsu_threshold(img, maxCount=255, verbose=False):
 
 	"""
 #######################  BEGIN ERROR CHECKING  #######################
-	if isinstance(img,np.ndarray) == True:
-		dims = ipcv.dimensions(img,"dictionary")
+	# if isinstance(img,np.ndarray) == True:
+	# 	dims = ipcv.dimensions(img,"dictionary")
 
-		if dims["bands"] == 1:
+	# 	if dims["bands"] == 1:
 
-			if len(img.shape) == 3:
-				#making the array two dimensional if it only has 1 band
-				img = img.reshape(dims['rows'],dims['cols'])
+	# 		if len(img.shape) == 3:
+	# 			#making the array two dimensional if it only has 1 band 
+	# 			img = img.reshape(dims['rows'],dims['cols'])
 
-		else:
-			print("")
-			print("input 'img' must be grayscale or single-banded")
-			print("")
-			raise RuntimeError
+	# 	else:
+	# 		print("")
+	# 		print("input 'img' must be grayscale or single-banded")
+	# 		print("")
+	# 		raise RuntimeError
 
-	else:
-		print("")
-		print("input 'img' must be a valid 2 dimensional numpy array, currently {0}".format(type(img)))
-		print("")
-		raise TypeError
+	# else:
+	# 	print("")
+	# 	print("input 'img' must be a valid 2 dimensional numpy array, currently {0}".format(type(img)))
+	# 	print("")
+	# 	raise TypeError
 
-	if isinstance(maxCount,int) == False:
-		print("")
-		print("input 'maxCount' must be an int, currently {0}".format(type(maxCount)))
-		print("")
-		raise TypeError
-	elif maxCount <= 0:
-		print("")
-		print("input 'maxCount' must be greater than zero")
-		print("")
-		raise ValueError
+	# if isinstance(maxCount,int) == False:
+	# 	print("")
+	# 	print("input 'maxCount' must be an int, currently {0}".format(type(maxCount)))
+	# 	print("")
+	# 	raise TypeError
+	# elif maxCount <= 0:
+	# 	print("")
+	# 	print("input 'maxCount' must be greater than zero")
+	# 	print("")
+	# 	raise ValueError
 
-	if isinstance(verbose,bool) == False:
-		print("")
-		print("input 'verbose' must be integer boolean , currently {0}".format(verbose))
-		print("")
-		raise TypeError
+	# if isinstance(verbose,bool) == False:
+	# 	print("")
+	# 	print("input 'verbose' must be integer boolean , currently {0}".format(verbose))
+	# 	print("")
+	# 	raise TypeError
 
-	if discriminant != "eta" and discriminant != 'lambda' and discriminant != 'kappa':
-		print("")
-		print("input 'discriminant' must equal to 'eta', 'lambda' or 'kappa'")
-		print("")
-		raise ValueError
+
 #######################  END ERROR CHECKING  #######################
 
 	try:
-		hist,pdf,cdf = ipcv.histogram(img,histSize=(maxCount+1),ranges=[0,maxCount+1])
-		muTotal = np.mean(cdf)
-		numberCounts = len(cdf)
 
+		numberCounts = maxCount + 1
+		#generating the pdf and cdf
+		hist,pdf,cdf = ipcv.histogram(img,histSize=numberCounts,ranges=[0,numberCounts])
+		
+		meanLevelArray = np.cumsum( np.arange(0,numberCounts) * pdf )
+		muTotal = meanLevelArray[-1]
 
-		sigmaSquaredBValues = []
-		for k in range( numberCounts ):
-			muK = np.mean(cdf[:k])
+		sigmaSquaredBValues = np.zeros(numberCounts)
+		startingK = np.where(cdf>0.0)[0][0]
+		endingK = np.where(cdf<1.0)[0][-1]
+
+		for k in range(startingK, endingK):
+			muK = meanLevelArray[k]
 			omegaK = cdf[k]
-			if omegaK != 0.0 and omegaK != 1.0:
-				sigmaSquaredB = ( ( (muTotal * omegaK) - muK )**2 ) / ( omegaK * (1-omegaK) )
-				sigmaSquaredBValues.append(sigmaSquaredB)
+			sigmaSquaredB = ( ( (muTotal * omegaK) - muK )**2 ) / ( omegaK * (1-omegaK) )
+			sigmaSquaredBValues[k] = sigmaSquaredB
 
-		kOptimal = np.argmax(sigmaSquaredBValues)
+		threshold = np.argmax(sigmaSquaredBValues)
 
 		LUT = np.zeros( numberCounts )
-		LUT[:kOptimal] = 0
-		LUT[kOptimal:] = 255
+		LUT[threshold+1:] = 1
 
-		img = LUT[img] 
+		img = LUT[img]
 
+		if verbose == True:
+			values = (pdf,)
+			colors = ('r',)
+			filename = "image_pdf_wOtsu.eps"
+			thresholdMarker = (threshold,)
+			labels = ("pdf",)
+			graph = ipcv.quickplot(values,colors,labels,filename=filename,verticalMarkers=thresholdMarker,\
+				xLabel="Digital Counts",yLabel="probability",display=False)
+			graph.annotate("otsu's Threshold", xy=(threshold, .01), xytext=(3, 1.5),arrowprops=dict(facecolor='black', shrink=0.05))
+			graph.show()
 
-		return img.astype(np.uint8), kOptimal
-
+		return img.astype(np.uint8), threshold
 
 
 	except Exception as e:
 		print("===============================================================")
 		exc_type, exc_obj, tb = exc_info()
 		fname = split(tb.tb_frame.f_code.co_filename)[1]
-		print("\nfile: {0}\n\nline: {1} \n\n{2}\n".format(fname,tb.tb_lineno,e))
+		print("\r\nfile: {0}\r\n\r\nline: {1} \r\n\r\n{2}\r\n\r\n".format(fname,tb.tb_lineno,exc_obj,e))
 		print("===============================================================")
 
 
@@ -139,7 +148,9 @@ if __name__ == '__main__':
 	 import time
 
 	 home = os.path.expanduser('~')
+	 # filename = home + os.path.sep + 'src/python/examples/data/lenna.tif'
 	 filename = home + os.path.sep + 'src/python/examples/data/giza.jpg'
+
 
 	 im = cv2.imread(filename, cv2.IMREAD_UNCHANGED)
 	 print('Filename = {0}'.format(filename))
@@ -148,7 +159,7 @@ if __name__ == '__main__':
 	 print('Image size = {0}'.format(im.size))
 
 	 startTime = time.time()
-	 thresholdedImage, threshold = ipcv.otsu_threshold(im, verbose=True)
+	 thresholdedImage, threshold = ipcv.otsu_threshold(im, verbose=False)
 	 print('Elapsed time = {0} [s]'.format(time.time() - startTime))
 
 	 print('Threshold = {0}'.format(threshold))
@@ -159,3 +170,72 @@ if __name__ == '__main__':
 	 cv2.imshow(filename + ' (Thresholded)', thresholdedImage * 255)
 
 	 action = ipcv.flush()
+
+
+
+  #                                          ,╓╖╗╗╗╣╣▄╦,                          
+  #                                 ,╓╦╗╬╣╬▓▓▓▓▓▓▓▓▓▓█▓▓▌⌐                        
+  #                               ╣▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓█████▓▓µ                       
+  #                               ▀█████▓▓▓▓▓▓▓▓▓▓▓██████▓▓▄                      
+  #                                ▀███████████▓▓▓▓█████████▓µ                    
+  #                                 ╙██████████████▓▓▓▓▓▓▓▓▓▓▓⌐                   
+  #                                   ▓████████████████▓▓▓▓▓▓▓▌                   
+  #                                    ╙█████████████████▓▓▓▓▓▓▒                  
+  #                                      ▐██████████████▓▓▓▓▓▓▓▓µ                 
+  #                                       ╫███████████▓▓▓▓▓▓▓▓▓▓▓⌐                
+  #                                       ╫███████████▓▓▓▓▓▓▓▓▓▓█▓⌐               
+  #                                      ▄████████████▓▓▓▓▓▓▓▓▓███▓               
+  #                                    ╓▓▓█████████████████▓▓▓▓▓███▓              
+  #                            ,,╗▄▓▓▓▓█▓███████████████████▓▓▓▓▓▓▓█▓⌐            
+  #                      ,╓╓╦╬▓▓▓▓█████████▀╠████████▓▓▓██████▓▓▓▓▓▓█▓            
+  #                 ╓╦╣▓▓▓▓▓▓▓▓▓▓███████▀`  ╫█████▓▓▓▓▓▓▓█▓▓████▓▓▓▓▓█▄,,╓▄       
+  #             ╥╬▓▓▓▓▓▓▓▓▓▓██████▓▀▀▀      ▓████▓▓▓▓▓▓▓▓█████▓▓█▓▓▓▓▓▓▓▓▀        
+  #         ,╗╣▓▓▓▓▓████████▀▀╙            ]█████▓▓▓▓▓▓▓▓█████▓▓▓▓▓▓▓██╨          
+  # ╦▄▄▄▓▓▓▓▓▓███▀▀▀▀▀▀╙                   ║████▓▓▓▓▓▓▓███████▓▓▓▓▓███▓           
+  # ╙╙▀▀████████▀                          ║███████▓▓▓████████▓▓▓█████▌           
+  #     ╙╙`╙▀▀                             '██████████████████████▓▓▓█Ö           
+  #                                         ╠█████████████████████▓▓▓▓▌⌐          
+  #                                        ,▓██████████████████████▓▓▓▓▓ç         
+  #                                       ╓▓███████████████████████▓▓▓▓▓▓▓╦       
+  #                                      ╬██████████████████████████▓▓▓▓▓▓▓µ      
+  #                                     ╫███████████████████████████▓▓▓▓▓▓█▓▄     
+  #                                    ▓████████████████████████████▓▓▓▓▓▓▓██µ    
+  #                                   ▓███████████████▀▀▀▓███████████▓▓▓▓▓▓▓█▌    
+  #                                  ╫█████████████▀▄▓███████████████▓▓▓▓▓▓▓▓█∩   
+  #                                ▄▓██████████▀▐▄████████████████████▓▓▓▓▓▓▓█▄   
+  #                              ,▓███████▀▀    ╫██████▓██▓▓▀▀▀▀▀▀▀▀▓█▓▓▓▓▓███▌   
+  #                              ╣██████▒               ▓▓          "▓█▓▓▓▓███▀   
+  #                             ╓▓▓▓█████╕              ▓▓           `▓█▓▓▓███∩   
+  #                              ▓█▓▓▓████▄             ▓▌            └▓▓▓▓███    
+  #                               ▀██▓▓▓████Q          ▐▓▌            ╓▓▓▓▓██▌    
+  #                                `▀▓███████▄         ╠╬▌           ║▓▓▓▓▓▓█∩    
+  #                                   ╙▀██▓███▓╕     ╓╬▓▌╬╬⌐        ╔▓▓▓▓▓▓▓█∩    
+  #                                      ╙▀▓████▌▄▄▄▄▓▓█▄╬╬▄,      ,▓▓▓▓▓▓███∩    
+  #                                         ▐▓███████████▒▓███▓▄,  ╬█▓▓▓▓▓██▌     
+  #                                       ╓▄█████████████▒▓███████▄▓▓▓▓▓▓███∩     
+  #                                     ╗▓█████████████▌╠╬▓█████████▓▓▓▓▓██▓      
+  #                                   ╓▓█████████▄▓██▓█▒╫╬▓▌█Ü▀████▓▓▓▓▓███∩      
+  #                                  ▄█████████████▓▓██Ö╠╬▓█▀  .▓██▓▓▓▓███▌       
+  #                                .▓████▓█╨  ╙██▌ ╠▓█▓ ╠╬█▓   ╫▓▓█▓▓▓▓███∩       
+  #                               ╓▓███▓█▓▓,   ╙██µ╠▓▓█⌐▌╬█∩  ╬█▀▓▓▓▓▓████        
+  #                              ╓▓███▓██▄▒▀▄    ██╠╬▓▓⌐▌╫▌  ▄█╨╓█▓▓▓▓████▌       
+  #                              ▓███▓█▀ ╙▀▓▄▓▄   ▓▓╬╫█µ╬▌  ▄▌  ╠█▓▓▓██████∩      
+  #                             ║████▓▌     ╙▓█▓⌐ ²█▓▓█▄╬▓╦▓▌  ╓▓▓▓▓▓██████▌      
+  #                             ████▓█▀▄▄,     ▀█▄ ╠████╬▓█▌ ╓▓▓▓▓▓▓██▀█████      
+  #                            ╓████▓█▓▄▄▓▓▓▄▄   ╙█▓▓█▓█╬▓▓▄▓╨ ╓▓▓▓██▓▓█████      
+  #                            ╠███▓█▒    ╙╙▀▀▓▓▓▄▓▓████╬▓█▄▄▄▄▓▓▓▓█▓╝╝█████      
+  #                            ▓███▓█           ;▓▓████▌▒▒╬╬▌╙╠▓▓▓█▓   █████      
+  #                            ▓███▓█╦▄▄╣▀▓▓███▓▀▀▒▓█████▓▓▀▓▓▓▓▓▓█▒  ┌█████      
+  #                            ▓███▓█▄▄╣▀▀▀`'  ,▄▓▀▓█████▒▓▄µ╠▓▓▓██▓▀▀▓████▌      
+  #                            ▀███▓█▌      ,▄██▀╓▓█╢███▓▒▌ ▀█▓▓██╨╙▀▀█████▒      
+  #                            ]██████   ╓▄▓▓▓╨ ╓██Ü▓█▓██▒▓▌╠▓▓▓█▒   ▄████▓       
+  #                             ▓███▓█▌▓▀▀▓▓╨  ▄██∩╔█▌╫█▓╬▓█▓▓▓█▓▀▄ ╓█████∩       
+  #                             ╚██████▄▓▀   ,▓▓▓  ▓█▒]█▓╫▒█▓▓▓█∩ `▓█████▀        
+  #                              ▀████▓█╦   ╓▓▄▓  ╫▓▓ ]█▓╫Q╣▓▓█▀  ▄█████▀         
+  #                               ▓██████▄ ╬▓▄▓  ╓█▓▌ ]█▌╫▒▓▓▓█▄╓▓█████▌          
+  #                                ▀███████▌▓▓   ▓▌▓∩  ▓▒╬▄▓█▓▓▓▓▓████▀           
+  #                                 ╙████████▄  ╓█╢▌   ▓▓███████████▀▀Θ           
+  #                                   ▀██████████▓▓▄,╓▄████████████╨              
+  #                                     ▀███████████████████████▀╨                
+  #                                       ╙▀█████████████████▀╙                   
+  #                                           ╙╙▀▀▓██▀▀▀▀▀                       
